bead0c9b052d9269f27d4f76cf3e6217d4388a60
walker27 <wzszhengzhou@hotmail.com>
2025-10-22 16:57:41 +0800

    chore: remove fonts

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,27 +2,11 @@ import { type AppType } from 'next/app'
 import { useEffect } from 'react'
 import Head from 'next/head'
 import { appWithTranslation } from 'next-i18next'
-import localFont from 'next/font/local'
 import { IS_PROD, api } from '../utils'
 import 'swiper/css'
 import '../styles/globals.scss'
 
-const fontArticulatCF = localFont({
-  src: [
-    { path: '../styles/fonts/ArticulatCF-Normal.woff2', weight: '400' },
-    { path: '../styles/fonts/ArticulatCF-Medium.woff2', weight: '500' },
-    { path: '../styles/fonts/ArticulatCF-DemiBold.woff2', weight: '600' },
-    { path: '../styles/fonts/ArticulatCF-Bold.woff2', weight: '700' },
-    { path: '../styles/fonts/ArticulatCF-ExtraBold.woff2', weight: '800' },
-  ],
-  preload: true,
-})
-
 const App: AppType = ({ Component, pageProps }) => {
-  useEffect(() => {
-    document.body.classList.add(fontArticulatCF.className)
-    return () => document.body.classList.remove(fontArticulatCF.className)
-  }, [])
 
   return (
     <>
@@ -37,10 +21,7 @@ const App: AppType = ({ Component, pageProps }) => {
           </>
         ) : null}
       </Head>
-      <main
-        // Here as redundancy in server-side rendering.
-        className={fontArticulatCF.className}
-      >
+      <main>
         <Component {...pageProps} />
       </main>
     </>
